8b11d2d3e82e2c781205a7ab818c840d6676f99a
bellanm23 <bella.nideni@students.utdi.ac.id>
2023-11-01 13:43:34 +0700

    Assignment Deployment Firabase

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://finalproject-galleryapp.web.app/" // TODO: replace this
+const studentName = "BELLA NIDENI MAHENDRA" // TODO: replace this
+const studentId = "FE2988347" // TODO: replace this
+
+export { url, studentName, studentId }
